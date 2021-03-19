8b0c0dc654aba8c7d6a3df50d39228e130da2e9f
mahun <mcintyre.mlm@gmail.com>
2021-03-19 04:00:39 -0400

    intro

--- components/ContentSection/index.tsx
+++ components/ContentSection/index.tsx
@@ -23,13 +23,14 @@ const ContentSection: React.FC<ContentSectionProps> = (
             Disney Streaming Services
           </Link>
           . I develop & maintain React applications written in TypeScript, REST
-          servers using Scala, & initiated/lead monthly education sessions that
-          promote collaboration as well as encourage other team members to
-          showcase topics they find interesting. When I'm not in the office I'm
-          either hanging out with my daughter Jane Mae, my dog Luna, friends &
-          family, or am on some type of adventure! My passions include reading,
-          working out, & traveling; from hiking up mountains (like Mt. Marcy),
-          to exploring foreign cities (such as Berlin).
+          servers using Scala, architect systems with AWS infrastrcture, promote
+          code changes with custom Jenkins pipelines & initiated/lead monthly
+          education sessions that promote collaboration with other team members
+          by showcasing topics they find interesting. When I'm not in the office
+          I'm either hanging out with my daughter Jane Mae, my dog Luna, friends
+          & family, or am on some type of adventure! My passions include
+          reading, working out, & traveling; from hiking up mountains (like Mt.
+          Marcy), to exploring foreign cities (such as Berlin).
         </p>
         <p>
           If you're interested in seeing some of the work I've done over the

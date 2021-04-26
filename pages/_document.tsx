e5cd27d0252a6eb201beb36cc0a43fe9a51f7a82
mahun <mcintyre.mlm@gmail.com>
2021-04-26 09:01:05 -0400

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,18 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0ME2VG1H10"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-0ME2VG1H10');`,
+            }}
+          />
         </body>
       </Html>
     );
